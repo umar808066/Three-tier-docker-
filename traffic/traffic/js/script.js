6ab705adc36f041a7d92d5168ec9821c4b3987e2
umar808066 <sk.umarriya66@gmail.com>
2024-08-29 12:55:17 +0530

    Add files via upload

---
+++ traffic/traffic/js/script.js
@@ -0,0 +1,26 @@
+function onStop() {
+    document.getElementById("stopLight").style.backgroundColor = "#cf1124";
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069";
+    document.getElementById("goLight").style.backgroundColor = "#4b5069";
+    document.getElementById("stopButton").style.backgroundColor = "#cf1124";
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41";
+}
+
+function onReady() {
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069";
+    document.getElementById("readyLight").style.backgroundColor = "#f7c948";
+    document.getElementById("goLight").style.backgroundColor = "#4b5069";
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("readyButton").style.backgroundColor = "#f7c948";
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41";
+}
+
+function onGo() {
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069";
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069";
+    document.getElementById("goLight").style.backgroundColor = "#199473";
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("goButton").style.backgroundColor = "#199473";
+}
